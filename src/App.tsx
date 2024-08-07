0aa58b29a20bbb50a71ba578358c70a064fe2cdb
bfaraway <152715944+bfaraway@users.noreply.github.com>
2024-08-07 20:08:51 +0200

    save playlist

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
-import Btn from './components/Btn'
-import SearchBar from './components/SearchBar'
-import Song from './components/Song'
-import Spotify from './services/spotify'
+import { useState } from "react";
+import Btn from "./components/Btn";
+import SearchBar from "./components/SearchBar";
+import Song from "./components/Song";
+import Spotify from "./services/spotify";
 
 type Song = {
   name: string;
@@ -16,6 +16,9 @@ type Song = {
 function App() {
   const [searchResults, setSearchResults] = useState<Song[]>([]);
   const [playlist, setPlaylist] = useState<Song[]>([]);
+  const [playlistName, setPlaylistName] = useState<string>(
+    "Rename your playlist"
+  );
 
   const handleSearch = async (query: string) => {
     const results = await Spotify.SearchSong(query);
@@ -23,30 +26,45 @@ function App() {
     setSearchResults(results);
   };
 
+  const handleSavePlaylist = async () => {
+    await Spotify.savePlaylist(playlistName, playlist.map((song) => song.uri));
+    setPlaylistName("Rename your playlist");
+    setPlaylist([]);
+  };
+
   const addToPlaylist = (song: Song) => {
     setPlaylist([...playlist, song]);
   };
 
   const removeFromPlaylist = (index: number) => {
-    setPlaylist(playlist.filter((_,i) => i !== index));
+    setPlaylist(playlist.filter((_, i) => i !== index));
+  };
+
+  const renamePlaylist = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPlaylistName(event.target.value);
   };
 
   return (
     <>
       <header className="flex items-center justify-between mb-6">
         <h1 className="text-2xl">Pure Playlists</h1>
-        <img src="/src/assets/spotify-logo.svg" alt="Spotify Logo" className="w-6 h-6 mr-2" />
+        <img
+          src="/src/assets/spotify-logo.svg"
+          alt="Spotify Logo"
+          className="w-6 h-6 mr-2"
+        />
       </header>
-      <div className="flex flex-col items-center justify-center rounded-lg p-4">
+      <div className="flex flex-col items-center justify-center rounded-md p-4">
         <div className="flex items-center justify-center mb-6 w-full">
           <SearchBar onSearch={handleSearch} />
         </div>
 
-
         {/* Search results and playlist */}
         {searchResults.length > 0 ? (
-        <div id="search-results" className="flex flex-row justify-between w-full rounded-lg p-4 bg-white">
-
+          <div
+            id="search-results"
+            className="flex flex-row justify-between w-full rounded-lg p-4 bg-white"
+          >
             <div className="flex flex-col justify-left mb-6 bg-white rounded-lg p-2 m-2 w-full">
               <p className="text-xl mb-4">Tracks</p>
               {searchResults.map((song, index) => (
@@ -63,8 +81,14 @@ function App() {
               ))}
             </div>
 
-            <div className="flex flex-col justify-right mb-6 bg-white rounded-lg p-2 m-2 w-full">
-              <p className="text-xl mb-4">Playlist</p>
+            <div className="flex flex-col mb-6 bg-white rounded-lg p-2 m-2 w-full">
+              <input
+                id="playlist-name"
+                className=" text-center rounded-md focus:outline-none focus:ring-1 focus:ring-black text-xl mb-4"
+                type="text"
+                onChange={renamePlaylist}
+                value={playlistName}
+              />
               {playlist.map((song, index) => (
                 <Song
                   key={index}
@@ -77,33 +101,48 @@ function App() {
                   previewUrl={song.previewUrl}
                 />
               ))}
+              <div className="flex items-center justify-center mt-4">
+                {/* Save playlist */}
+                {playlist.length > 0 && (
+                  <div
+                    id="save-playlist"
+                    className="flex items-right justify-right mt-4"
+                  >
+                    <Btn text={`Save playlist on Spotify`} onClick={handleSavePlaylist} />
+                  </div>
+                )}
+              </div>
             </div>
-          
-        </div>
-
-        ) : (
-
-        // Empty state
-        <div id="empty-state" className=" h-96 flex flex-col items-center justify-center w-full rounded-lg p-4 bg-white">
-          <p>No tracks found, start with searching for a track</p>
-          <img id="empty-state-image" src="/src/assets/vinyl-record.svg" alt="Empty state" className="w-8 h-8 mr-2 animate-spin mt-4" />
           </div>
-        )}
-
-
-        {/* Save playlist */}
-        {playlist.length > 0 && (
-          <div id="save-playlist" className="flex items-right justify-right mt-4"> 
-            <Btn text={`Save playlist on Spotify`} />
+        ) : (
+          // Empty state
+          <div
+            id="empty-state"
+            className=" h-96 flex flex-col items-center justify-center w-full rounded-lg p-4 bg-white"
+          >
+            <p>No tracks found, start with searching for a track</p>
+            <img
+              id="empty-state-image"
+              src="/src/assets/vinyl-record.svg"
+              alt="Empty state"
+              className="w-8 h-8 mr-2 animate-spin mt-4"
+            />
           </div>
         )}
       </div>
 
       <p className="flex bottom-0 items-center justify-center color-gray-800 mt-11 ">
-        Made by <a className="text-blue-500 ml-1" href="https://github.com/bfaraway" target="_blank">Magnus Bjelland</a>
+        Made by{" "}
+        <a
+          className="text-blue-500 ml-1"
+          href="https://github.com/bfaraway"
+          target="_blank"
+        >
+          Magnus Bjelland
+        </a>
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/services/spotify.tsx
+++ src/services/spotify.tsx
@@ -6,65 +6,113 @@ const authUrl = "https://accounts.spotify.com/authorize";
 const baseUrl = "https://api.spotify.com/v1";
 
 const Spotify = {
-
-    getAccessToken: () => {
-        const accessToken = localStorage.getItem('spotifyAccessToken');
-        const expiryTime = localStorage.getItem('spotifyTokenExpiry');
-        console.log(redirectUri)
-        if (accessToken && expiryTime && Number(expiryTime) > Date.now()) {
-            return accessToken;
-        }
-
-        const accessTokenMatch = window.location.hash.match(/access_token=([^&]*)/);
-        const expiresInMatch = window.location.hash.match(/expires_in=([^&]*)/);
-
-        if (accessTokenMatch && expiresInMatch) {
-            const newAccessToken = accessTokenMatch[1];
-            const expiresIn = Number(expiresInMatch[1]);
-            const newExpiryTime = Date.now() + expiresIn * 1000;
-
-            localStorage.setItem('spotifyAccessToken', newAccessToken);
-            localStorage.setItem('spotifyTokenExpiry', newExpiryTime.toString());
-
-            window.location.hash = '';
-            return newAccessToken;
-        } else {
-            const scope = 'user-read-private user-read-email playlist-modify-public';
-            window.location.href = `${authUrl}?client_id=${spotifyClientId}&response_type=token&redirect_uri=${redirectUri}&scope=${encodeURIComponent(scope)}`;
-        }
-    },
-
-    SearchSong: async (songName: string) => {
-        const accessToken = await Spotify.getAccessToken();
-        try {
-            const response = await fetch(`${baseUrl}/search?q=${songName}&type=track&limit=10`, {
-                method: "GET",
-                headers: {
-                    "Authorization": `Bearer ${accessToken}`
-                }
-            });
-
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-
-            const Songs = await response.json();
-            console.log(Songs);
-            return Songs.tracks.items.map((song: any) => ({
-                id: song.id,
-                name: song.name,
-                artist: song.artists[0].name,
-                album: song.album.name,
-                uri: song.uri,
-                previewUrl: song.preview_url
-            })); 
-            
-        } catch (error) {
-            console.error("Failed to search for song:", error);
-            throw error; // Re-throw the error so it can be handled by the caller
-        }   
+  getAccessToken: () => {
+    const accessToken = localStorage.getItem("spotifyAccessToken");
+    const expiryTime = localStorage.getItem("spotifyTokenExpiry");
+    console.log(redirectUri);
+    if (accessToken && expiryTime && Number(expiryTime) > Date.now()) {
+      return accessToken;
     }
 
+    const accessTokenMatch = window.location.hash.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.hash.match(/expires_in=([^&]*)/);
+
+    if (accessTokenMatch && expiresInMatch) {
+      const newAccessToken = accessTokenMatch[1];
+      const expiresIn = Number(expiresInMatch[1]);
+      const newExpiryTime = Date.now() + expiresIn * 1000;
+
+      localStorage.setItem("spotifyAccessToken", newAccessToken);
+      localStorage.setItem("spotifyTokenExpiry", newExpiryTime.toString());
+
+      window.location.hash = "";
+      return newAccessToken;
+    } else {
+      const scope = "user-read-private user-read-email playlist-modify-public";
+      window.location.href = `${authUrl}?client_id=${spotifyClientId}&response_type=token&redirect_uri=${redirectUri}&scope=${encodeURIComponent(
+        scope
+      )}`;
+    }
+  },
+
+  SearchSong: async (songName: string) => {
+    const accessToken = await Spotify.getAccessToken();
+    try {
+      const response = await fetch(
+        `${baseUrl}/search?q=${songName}&type=track&limit=10`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const Songs = await response.json();
+      console.log(Songs);
+      return Songs.tracks.items.map((song: any) => ({
+        id: song.id,
+        name: song.name,
+        artist: song.artists[0].name,
+        album: song.album.name,
+        uri: song.uri,
+        previewUrl: song.preview_url,
+      }));
+    } catch (error) {
+      console.error("Failed to search for song:", error);
+      throw error; // Re-throw the error so it can be handled by the caller
+    }
+  },
+
+  savePlaylist: async (playlistName: string, songUris: string[]) => {
+    if (!playlistName || songUris.length === 0) {
+      return;
+    }
+
+    const accessToken = Spotify.getAccessToken();
+    const headers = { Authorization: `Bearer ${accessToken}` };
+
+    try {
+      // Get the user's Spotify ID
+      const userResponse = await fetch(`${baseUrl}/me`, { headers });
+      const userData = await userResponse.json();
+      const userId = userData.id;
+
+      // Create a new playlist
+      const createPlaylistResponse = await fetch(
+        `${baseUrl}/users/${userId}/playlists`,
+        {
+          method: "POST",
+          headers: {
+            ...headers,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ name: playlistName }),
+        }
+      );
+      const playlistData = await createPlaylistResponse.json();
+      const playlistId = playlistData.id;
+
+      // Add tracks to the playlist
+      await fetch(`${baseUrl}/playlists/${playlistId}/tracks`, {
+        method: "POST",
+        headers: {
+          ...headers,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ uris: songUris }),
+      });
+
+      return playlistId;
+    } catch (error) {
+      console.error("Error saving playlist:", error);
+      throw error;
+    }
+  },
 };
 
 export default Spotify;
